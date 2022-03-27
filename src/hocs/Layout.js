bdee0a934847bf33e4d42af72114edd3183cd26a
Torr1co <fabriziotorrico27@gmail.com>
2022-03-26 21:58:36 -0300

    solving warnings for prof

--- src/hocs/Layout.js
+++ src/hocs/Layout.js
@@ -1,4 +1,4 @@
-import Head from "next/Head";
+import Head from "next/head";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import { useAuth } from "../firebase/auth";
