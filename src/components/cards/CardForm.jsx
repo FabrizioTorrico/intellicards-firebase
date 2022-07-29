0987b95ca7becd931dd641087fa6b45d411c6dae
fabrizioTorrico <fabrizio@zil.global>
2022-07-28 23:16:46 -0300

    showing back and front

--- src/components/cards/CardForm.jsx
+++ src/components/cards/CardForm.jsx
@@ -8,6 +8,8 @@ import {
   Button,
   Grid,
   Box,
+  Text,
+  Flex,
 } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import ImageUploader from '../ImageUploader'
@@ -15,11 +17,18 @@ import toast from 'react-hot-toast'
 import { useRouter } from 'next/router'
 import { useForm } from 'react-hook-form'
 import { createCard } from '../../firebase/firestore'
+import Divider from '../Divider'
 
 const TextInput = React.forwardRef(function TextInput(props, ref) {
+  const error = props.errors.front?.message || props.errors.back?.message
   return (
-    <FormControl id={props.id} isInvalid={props.errors[props.id]}>
+    <FormControl
+      id={props.id}
+      isInvalid={error}
+      hidden={props.id !== props.cardFace}
+    >
       <Textarea
+        border={'none'}
         id={props.id}
         placeholder={props.label}
         _placeholder={{ fontSize: 'xl' }}
@@ -28,14 +37,20 @@ const TextInput = React.forwardRef(function TextInput(props, ref) {
         h={'44vh'}
         maxH={'44vh'}
       />
-      <FormErrorMessage>{props.errors[props.id]?.message}</FormErrorMessage>
+      <FormErrorMessage>{error}</FormErrorMessage>
     </FormControl>
   )
 })
 
 export default function CardForm() {
   const [triggerAnimation, setTriggerAnimation] = useState(false)
-  // const [cardFace, setCardFace] = useState('front')
+  const [cardFace, setCardFace] = useState('front')
+  const [preview, setPreview] = useState(false)
+  const previewStyleText = {
+    color: 'main.500',
+    borderBottom: '2px solid transparent',
+    borderColor: 'main.500',
+  }
   const router = useRouter()
   const { deckId } = router.query
   const {
@@ -45,10 +60,12 @@ export default function CardForm() {
     reset,
     clearErrors,
     formState: { errors },
-  } = useForm()
-
+  } = useForm({
+    front: '',
+    back: '',
+  })
+  console.log(errors)
   const onSubmit = async (data) => {
-    data.back = 'asdasd'
     toast
       .promise(createCard(deckId, data), {
         loading: <b>Creating card...</b>,
@@ -64,24 +81,57 @@ export default function CardForm() {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <Box
-        py={16}
+        pb={16}
+        pt={4}
         px={8}
         h="64vh"
         className={triggerAnimation ? styles.fade_in : ''}
         onAnimationEnd={() => setTriggerAnimation(false)}
         // overflow={'auto'}
       >
+        <Flex pl={4} gap={8}>
+          <Text
+            p={3}
+            cursor={'pointer'}
+            variant="link"
+            {...(!preview ? previewStyleText : {})}
+            onClick={() => setPreview(false)}
+          >
+            Editing {cardFace[0].toUpperCase() + cardFace.substring(1)}
+          </Text>
+          <Text
+            p={3}
+            cursor={'pointer'}
+            variant="link"
+            {...(preview ? previewStyleText : {})}
+            onClick={() => setPreview(true)}
+          >
+            Preview
+          </Text>
+        </Flex>
+        <Divider />
         <TextInput
-          {...register('cardFace', {
-            required: 'front is required',
+          {...register('front', {
+            required: `Front is required.`,
             maxLength: { value: 1500, message: 'Max length is 1500 char' },
           })}
           errors={errors}
           label={'Start writing'}
-          id="front"
+          id={'front'}
+          cardFace={cardFace}
+        />
+        <TextInput
+          {...register('back', {
+            required: `Back is required.`,
+            maxLength: { value: 1500, message: 'Max length is 1500 char' },
+          })}
+          errors={errors}
+          label={'Start writing'}
+          id={'back'}
+          cardFace={cardFace}
         />
       </Box>
-      {/* <Divider width="100%" borderWidth="2px" /> */}
+
       <Grid mx={8} gap={4} templateColumns={'repeat(4, 1fr)'}>
         <FormControl>
           <FormLabel htmlFor="type">Type: </FormLabel>
@@ -96,24 +146,19 @@ export default function CardForm() {
           <FormErrorMessage>{errors.image?.message}</FormErrorMessage>
         </FormControl>
         {/* <GridItem alignSelf="end" colSpan={2}> */}
-        <Button colorScheme="main" alignSelf="end">
-          Show Back
+        <Button
+          colorScheme="main"
+          alignSelf="end"
+          onClick={() =>
+            setCardFace((curFace) => (curFace === 'front' ? 'back' : 'front'))
+          }
+        >
+          Show {cardFace === 'front' ? 'Back' : 'Front'}
         </Button>
         <Button colorScheme="main" type="submit" alignSelf="end">
           Create
         </Button>
       </Grid>
-      {/* <TextInput
-            {...register('back', {
-              required: 'back is required',
-              maxLength: { value: 1500, message: 'Max length is 1500 char' },
-            })}
-            errors={errors}
-            label="Back"
-            id="back"
-          /> */}
-
-      {/* </GridItem> */}
     </form>
   )
 }
--- src/components/cards/CardList.jsx
+++ src/components/cards/CardList.jsx
@@ -1,11 +1,32 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Flex, Text } from '@chakra-ui/react'
 import CardPreview from './CardPreview'
 import CardPreviewAdmin from './CardPreviewAdmin'
 import { useCard } from './CardContext'
-
+import styles from '../../styles/Animations.module.scss'
+import { ArrowForwardIcon } from '@chakra-ui/icons'
 export default function CardList({ deckId, admin }) {
   const { cards } = useCard()
 
+  const renderCardList = () => {
+    if (!cards || (Array.isArray(cards) && cards.length === 0))
+      return (
+        <Flex flexDirection={'column'} alignItems={'center'} gap={6}>
+          <Text color="gray.600" fontSize={{ base: 'lg', md: '2xl' }}>
+            Create your first card!
+          </Text>
+          <ArrowForwardIcon
+            color="gray.600"
+            w={8}
+            h={8}
+            className={styles.bounceX}
+          />
+        </Flex>
+      )
+
+    return cards.map((card, i) => (
+      <CardPreview key={i} index={i} deckId={deckId} cardData={card} />
+    ))
+  }
   return (
     <Box
       position="fixed"
@@ -17,9 +38,7 @@ export default function CardList({ deckId, admin }) {
     >
       <Flex py={8} px={6} gap={6} direction={'column'}>
         {admin && <CardPreviewAdmin />}
-        {cards.map((card, i) => (
-          <CardPreview key={i} index={i} deckId={deckId} cardData={card} />
-        ))}
+        {renderCardList()}
       </Flex>
     </Box>
   )
